668863e9d05075a9c8d6b56ccb6d5431877c64d8
wjrelumba <williamjamesrelumba@iskolarngbayan.pup.edu.ph>
2022-11-21 00:46:37 +0800

    All bugs are fixed, and the game is now working properly.

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolDown = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown <- 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = 1;
+        }
+        if (coolDown >= 0)
+        {
+            coolDown -= Time.deltaTime;
+        }
+    }
+}
